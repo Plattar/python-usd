25954a90b9fd67eebbf545a339d29b471a20ea44
David Arayan <david.arayan12@gmail.com>
2022-07-05 12:51:05 +1000

    - Modify marc-build file to include buildx builder creation

--- marc-build.sh
+++ marc-build.sh
@@ -1,1 +1,3 @@
+docker buildx create --name plattarMarcBuilder
+docker buildx use plattarMarcBuilder
 docker buildx build --push --tag plattar/python-usd:version-$1 --platform linux/amd64,linux/arm64 --file Dockerfile .
